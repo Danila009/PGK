af75ef115617580d04e2aa211b28271685717ebd
210User <210User@pgk63.ru>
2022-12-22 11:17:14 +0400

    student list screen

--- PGK.Android/core/core-network/src/main/java/ru/pgk63/core_common/api/student/StudentApi.kt
+++ PGK.Android/core/core-network/src/main/java/ru/pgk63/core_common/api/student/StudentApi.kt
@@ -1,15 +1,16 @@
-package ru.pgk63.core_common.api.student.model
+package ru.pgk63.core_common.api.student
 
 import retrofit2.http.GET
 import retrofit2.http.Query
 import ru.pgk63.core_common.Constants
+import ru.pgk63.core_common.api.student.model.StudentResponse
 
 interface StudentApi {
 
-    @GET
+    @GET("/pgk63/api/Student")
     suspend fun getAll(
         @Query("search") search:String? = null,
         @Query("pageNumber") pageNumber: Int = 1,
         @Query("pageSize") pageSize: Int = Constants.PAGE_SIZE
-    )
+    ): StudentResponse
 }
--- PGK.Android/core/core-network/src/main/java/ru/pgk63/core_common/api/student/repository/StudentRepository.kt
+++ PGK.Android/core/core-network/src/main/java/ru/pgk63/core_common/api/student/repository/StudentRepository.kt
@@ -1,11 +1,17 @@
 package ru.pgk63.core_common.api.student.repository
 
+import ru.pgk63.core_common.Constants
 import ru.pgk63.core_common.api.student.StudentApi
 import ru.pgk63.core_common.common.response.ApiResponse
 import javax.inject.Inject
 
-class StudentRepsitory @Inject constructor(
+class StudentRepository @Inject constructor(
     private val studentApi: StudentApi
 ): ApiResponse() {
-    
+
+    suspend fun getAll(
+        search:String? = null,
+        pageNumber: Int = 1,
+        pageSize: Int = Constants.PAGE_SIZE
+    ) = studentApi.getAll(search, pageNumber, pageSize)
 }
