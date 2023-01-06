63fca01f86c3060a4d93e1802a2eec7d43472a1c
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2023-01-07 01:04:47 +0400

    send email updated password

--- PGK.Backend/PGK.Application/App/User/Commands/EmailPaswordReset/EmailPaswordResetCommandHandler.cs
+++ PGK.Backend/PGK.Application/App/User/Commands/EmailPaswordReset/EmailPaswordResetCommandHandler.cs
@@ -5,6 +5,7 @@ using PGK.Application.Common;
 using PGK.Application.Common.Exceptions;
 using PGK.Application.Interfaces;
 using PGK.Application.Security;
+using PGK.Application.Services.EmailService;
 using System.Net;
 
 namespace PGK.Application.App.User.Commands.EmailPaswordReset
@@ -13,10 +14,11 @@ namespace PGK.Application.App.User.Commands.EmailPaswordReset
         : IRequestHandler<EmailPaswordResetCommand, ContentResult>
     {
         private readonly IPGKDbContext _dbContext;
+        private readonly IEmailService _emailService;
         private readonly IAuth _auth;
 
-        public EmailPaswordResetCommandHandler(IPGKDbContext dbContext,
-            IAuth auth) => (_dbContext, _auth) = (dbContext, auth);
+        public EmailPaswordResetCommandHandler(IPGKDbContext dbContext, IEmailService emailService,
+            IAuth auth) => (_dbContext,_emailService, _auth) = (dbContext,emailService, auth);
 
         public async Task<ContentResult> Handle(EmailPaswordResetCommand request,
             CancellationToken cancellationToken)
@@ -48,20 +50,25 @@ namespace PGK.Application.App.User.Commands.EmailPaswordReset
             user.SendEmailToken = null;
             await _dbContext.SaveChangesAsync(cancellationToken);
 
-            var date = DateTime.Now.Date;
-
             var html = File.ReadAllText("Html/email_addres_password_reset_web.html");
 
             html = html.Replace("{{username}}", user.FirstName);
             html = html.Replace("{{password}}", newPassword);
 
-            if ((date.Month == 12 && date.Day >= 20) || (date.Month == 1 && date.Day <= 15))
+            html = html.Replace("{{image_src}}", GetPgkIconUrl());
+
+            if(user.Email != null)
             {
-                html = html.Replace("{{image_src}}", $"{Constants.BASE_URL}/Image/new_year_pgk_icon.png");
-            }
-            else
-            {
-                html = html.Replace("{{image_src}}", $"{Constants.BASE_URL}/Image/pgk_icon.png");
+                var sentEmailMessageHtml = File.ReadAllText("Html/send_email_new_password.html");
+
+                sentEmailMessageHtml = sentEmailMessageHtml.Replace("{{username}}", user.FirstName);
+                
+                sentEmailMessageHtml = sentEmailMessageHtml.Replace("{{image_src}}", GetPgkIconUrl());
+
+                await _emailService.SendEmailAsync(
+                email: user.Email,
+                subject: "Пароль успешно изменен",
+                message: html);
             }
 
             return new ContentResult
@@ -70,6 +77,20 @@ namespace PGK.Application.App.User.Commands.EmailPaswordReset
                 Content = html,
                 StatusCode = (int)HttpStatusCode.OK,
             };
+        }
+
+        private string GetPgkIconUrl()
+        {
+            var date = DateTime.Now.Date;
+
+            if ((date.Month == 12 && date.Day >= 20) || (date.Month == 1 && date.Day <= 15))
+            {
+                return $"{Constants.BASE_URL}/Image/new_year_pgk_icon.png";
+            }
+            else
+            {
+                return $"{Constants.BASE_URL}/Image/pgk_icon.png";
+            }
         }
     }
 }
